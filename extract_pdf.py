ce3703c4f25cd13d04b09340ef6e0481588157f0
Leyan365 <anujpathirage8959@gmail.com>
2026-08-30 14:43:04 +0530

    Repository cleanup before final submission

--- extract_pdf.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-with pdfplumber.open("Production AI Tutorial.pdf") as pdf:
-    text = ""
-    for page in pdf.pages:
-        text += page.extract_text() + "\n"
-with open("pdf_text.txt", "w", encoding="utf-8") as out:
-    out.write(text)
